008061b699ff710d259d9f44207de6d970dc947e
Ankur Mursalin <mir.ankur.ruet13@gmail.com>
2024-12-27 09:44:10 +0600

    feat: sharing data to specific view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::share('appName', 'Laravel Example App');
+
+        view()->composer('partials.navbar', function ($view) {
+            $view->with('pageTitle', 'Welcome');
+        });
     }
 }
